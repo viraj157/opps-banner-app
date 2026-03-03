db32d33abc9b86d38ab250d34b6448df8ad2a867
Viraj Chitlangia <virajchitlangia15@gmail.com>
2026-03-03 09:11:19 +0530

    Feature (UC4): Print OOPS to console

---
+++ src/com.oopsbanner/OOPSBannerApp4.java
@@ -0,0 +1,19 @@
+public class OOPSBannerApp4 {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            " *****    *****    ******    ***** ",
+            "**   **  **   **  **   **  **      ",
+            "**   **  **   **  **   **  *****   ",
+            "**   **  **   **  ******       **  ",
+            "**   **  **   **  **           **  ",
+            "**   **  **   **  **      **   **  ",
+            " *****    *****   **       *****   "
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
